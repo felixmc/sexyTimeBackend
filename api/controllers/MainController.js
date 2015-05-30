e2ffc3b1d1255ab606af4e1664040ef4ccacf29c
Felix Milea-Ciobanu <felixmilea@gmail.com>
2015-05-30 03:00:03 -0600

    testing

---
+++ api/controllers/MainController.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var MainController = {
+	rate: function(req, res) {
+		Photo.findOne();
+	}
+};
+
+module.exports = MainController;
--- api/models/User.js
+++ api/models/User.js
@@ -7,7 +7,7 @@ var User = {
 //		email         : { type: 'string', unique: true, required: true },
 //		password_hash : { type: 'string' },
 //		password_salt : { type: 'string' },
-		rating_average: { type: 'float' },
+		rating_average: { type: 'float', defaultsTo: 0 },
 		gender        : { type: 'string', enum: ['m', 'f'], required: true },
 		wants_gender  : { type: 'array', enum: ['m', 'f'], required: true }//,
 //		date_created  : { type: 'datetime' },
