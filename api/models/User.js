d1722ac4e72c32ec695070f4dd4f58e23b512d82
Felix Milea-Ciobanu <felixmilea@gmail.com>
2015-05-30 04:22:04 -0600

    testing

--- api/models/User.js
+++ api/models/User.js
@@ -38,9 +38,8 @@ var UserModel = {
 	},
 
 	authenticate: function(username, password, cb) {
-		User.find({ username: username }, function(err, user) {
+		User.findOne({ username: username }, function(err, user) {
 			if (err) sails.log.error(err);
-			console.log(user);
 			if (user && user.validPassword(password)) cb(true)
 			else cb(false)
 		});
