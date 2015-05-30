2d2ea13e60b2f1a18c416b6b7c971eb8b1db75fa
Felix Milea-Ciobanu <felixmilea@gmail.com>
2015-05-30 02:53:35 -0600

    controllers

---
+++ api/controllers/RatingController.js
@@ -0,0 +1,11 @@
+/**
+ * RatingController
+ *
+ * @description :: Server-side logic for managing ratings
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
